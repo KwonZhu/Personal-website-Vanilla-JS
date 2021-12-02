2b77f956d1631814a3999f3fa10f259396c1baa4
kwonz <kwonzhu@gmail.com>
2021-12-02 21:16:07 +1030

    add clicks in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,32 @@ $(document).ready(function() {
   $(".resume__page").hide();
   $(".services__page").hide();
   $(".contact__page").hide();
+  //click home
+  $(".tab__home").click(function(){
+    $(".home__page").show();
+    $(".resume__page").hide();
+    $(".services__page").hide();
+    $(".contact__page").hide();
+  });
+  //click resume
+  $(".tab__resume").click(function(){
+    $(".resume__page").show();
+    $(".home__page").hide();
+    $(".services__page").hide();
+    $(".contact__page").hide();
+  });
+  //click services
+  $(".tab__services").click(function(){
+    $(".services__page").show();
+    $(".home__page").hide();
+    $(".resume__page").hide();
+    $(".contact__page").hide();
+  });
+  //click contact
+  $(".tab__contact").click(function(){
+   $(".contact__page").show();
+   $(".home__page").hide();
+   $(".resume__page").hide();
+   $(".services__page").hide();
+  });
 });
